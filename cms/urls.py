965b69dfd002087670ea1c898d31a35419ddd8c0
kirshuvl <kirshuvl@gmail.com>
2023-02-18 02:05:28 +0400

    BugFix

--- cms/urls.py
+++ cms/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('cms.feedback.urls')),
-    path('cms/', include('cms.groups.urls')),
+    #path('', include('cms.feedback.urls')),
+    #path('cms/', include('cms.groups.urls')),
     path('cms/', include('cms.other.urls')),
 ]
